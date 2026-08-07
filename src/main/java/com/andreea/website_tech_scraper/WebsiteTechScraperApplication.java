d77ed577fe139d5458acb7a6f22207369bc7576e
Andritchii Andreea <adyandreea48@gmail.com>
2026-08-07 19:26:11 +0300

    Configure project dependecies

---
+++ src/main/java/com/andreea/website_tech_scraper/WebsiteTechScraperApplication.java
@@ -0,0 +1,13 @@
+package com.andreea.website_tech_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteTechScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteTechScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreea/website_tech_scraper/WebsiteTechScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreea.website_tech_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteTechScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
