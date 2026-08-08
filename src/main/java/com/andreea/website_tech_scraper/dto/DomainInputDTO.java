93bb34a179fce3d89d430e8f34e1c0ad1f46bd5a
Andritchii Andreea <adyandreea48@gmail.com>
2026-08-08 12:28:12 +0300

    Add DomainInputDTO and ResourceLoaderService for reading input files

---
+++ src/main/java/com/andreea/website_tech_scraper/dto/DomainInputDTO.java
@@ -0,0 +1,10 @@
+package com.andreea.website_tech_scraper.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class DomainInputDTO {
+    @JsonProperty("root_domain")
+    private String rootDomain;
+}
---
+++ src/main/java/com/andreea/website_tech_scraper/service/ResourceLoaderService.java
@@ -0,0 +1,38 @@
+package com.andreea.website_tech_scraper.service;
+
+import com.andreea.website_tech_scraper.dto.DomainInputDTO;
+import com.andreea.website_tech_scraper.dto.TechRule;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ResourceLoaderService {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<TechRule> loadRules(String rulesFileName) throws IOException {
+        ClassPathResource resource = new ClassPathResource(rulesFileName);
+        try (InputStream is = resource.getInputStream()) {
+            return objectMapper.readValue(is, new TypeReference<List<TechRule>>() {});
+        }
+    }
+
+    public List<String> loadDomains(String domainsFileName) throws IOException {
+        ClassPathResource resource = new ClassPathResource(domainsFileName);
+        try (InputStream is = resource.getInputStream()) {
+            List<DomainInputDTO> domainInputs = objectMapper.readValue(is, new TypeReference<List<DomainInputDTO>>() {
+            });
+
+            return domainInputs.stream()
+                    .map(DomainInputDTO::getRootDomain)
+                    .collect(Collectors.toList());
+        }
+    }
+}
